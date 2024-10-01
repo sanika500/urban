1ba0bda8c79857619e745a84e06f55e07acb09fc
Sanika500 <sanikasanthosh500@gmail.com>
2024-10-01 09:00:54 +0530

    done

--- urban/mainapp/models.py
+++ urban/mainapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class product(models.Model):
+      name=models.CharField(max_length=50)
+      stock=models.IntegerField()
+      description=models.CharField(max_length=100)
+      price=models.IntegerField()
+      accessories=models.CharField(max_length=50)
+      bookingamount=models.IntegerField()
+      image=models.ImageField()
+      
